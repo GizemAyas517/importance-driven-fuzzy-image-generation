cbb145cc2c88e621ab6ffcd6b4e7e770c7e5191b
GizemAyas517 <24607637+GizemAyas517@users.noreply.github.com>
2023-11-19 21:46:42 +0300

    refactored unblurring code

--- unblur.py
+++ unblur.py
@@ -1,140 +1,65 @@
 import time
-import sys
-import pprint
-import numpy as np
+import os
 from PIL import Image
 import torch
 import torchvision.transforms as transforms
-
-sys.path.append(".")
-sys.path.append("..")
 
 from utils.common import tensor2im
 from utils.inference_utils import run_inversion
 from utils.model_utils import load_model
 
 
-EXPERIMENT_DATA_ARGS = {
-    "cars": {
-        "model_path": "/content/drive/MyDrive/hyperstyle_cars.pt",
-        "w_encoder_path": "/content/drive/MyDrive/cars_w_encoder.pt",
-        "image_path": "/content/drive/MyDrive/whole_blurred/",
+"""
+    Hyperstyle code is taken from https://github.com/yuval-alaluf/hyperstyle
+
+    Clone the repository from https://github.com/yuval-alaluf/hyperstyle.git
+"""
+
+path_to_model = "/content/.../hyperstyle_cars.pt"
+path_to_w_encoder = "/content/.../cars_w_encoder.pt"
+dataset_path = "/content/.../whole_blurred/"
+
+def unblur_with_hyperstyle(dataset_path, destination_path):
+  EXPERIMENT_DATA_ARGS = {
+      "cars": {
+        "model_path": path_to_model,
+        "w_encoder_path": path_to_w_encoder,
+        "image_path": dataset_path,
         "transform": transforms.Compose([
             transforms.Resize((192, 256)),
             transforms.ToTensor(),
             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+        }
     }
-}
 
-#@title Load HyperStyle Model { display-mode: "form" } 
-model_path = EXPERIMENT_ARGS['model_path']
-net, opts = load_model(model_path, update_opts={"w_encoder_checkpoint_path": EXPERIMENT_ARGS['w_encoder_path']})
-print('Model successfully loaded!')
-pprint.pprint(vars(opts))
+  experiment_type = "cars"
+  EXPERIMENT_ARGS = EXPERIMENT_DATA_ARGS[experiment_type]
 
-
-def get_coupled_results(result_batch, transformed_image):
-    result_tensors = result_batch[0]  # there's one image in our batch
-    final_rec = tensor2im(result_tensors[-1]).resize(resize_amount)
-    input_im = tensor2im(transformed_image).resize(resize_amount)
-    res = np.concatenate([np.array(input_im), np.array(final_rec)], axis=1)
-    res = Image.fromarray(res)
-    return res
-
-def get_unblurred(result_batch, transformed_image):
-    result_tensors = result_batch[0]  # there's one image in our batch
-    final_rec = tensor2im(result_tensors[-1]).resize(resize_amount)
-    return final_rec
-
-# unblur whole image
-
-for i in range(8144):
-  image_path = EXPERIMENT_DATA_ARGS[experiment_type]["image_path"]+"whole_blurred"+str(i)+".jpg"
-  original_image = Image.open(image_path).convert("RGB")
-  original_image = original_image.resize((192, 256))
-
-  input_image = original_image
-  input_image.resize((256, 256))
+  if not os.path.exists(EXPERIMENT_ARGS['model_path']) or os.path.getsize(EXPERIMENT_ARGS['model_path']) < 1000000:
+    """
+        download this file
+        downloader.download_file(file_id=HYPERSTYLE_PATHS[experiment_type]['id'], file_name=HYPERSTYLE_PATHS[experiment_type]['name'])
+    """
+    # if google drive receives too many requests, we'll reach the quota limit and be unable to download the model
+    if os.path.getsize(EXPERIMENT_ARGS['model_path']) < 1000000:
+        raise ValueError("Pretrained model was unable to be downloaded correctly!")
+    else:
+        print('Done.')
+  else:
+        print(f'HyperStyle model for {experiment_type} already exists!')
 
 
-  n_iters_per_batch = 5
-  opts.n_iters_per_batch = n_iters_per_batch
-  opts.resize_outputs = False  # generate outputs at full resolution
+  model_path = EXPERIMENT_ARGS['model_path']
+  net, opts = load_model(model_path, update_opts={"w_encoder_checkpoint_path": EXPERIMENT_ARGS['w_encoder_path']})
+  print('Model successfully loaded!')
 
-  img_transforms = EXPERIMENT_ARGS['transform']
-  transformed_image = img_transforms(input_image) 
-
-  with torch.no_grad():
-    tic = time.time()
-    result_batch, result_latents, _ = run_inversion(transformed_image.unsqueeze(0).cuda(), 
-                                                    net, 
-                                                    opts,
-                                                    return_intermediate_results=True)
-    toc = time.time()
-    print('Inference took {:.4f} seconds.'.format(toc - tic))
-  resize_amount = (256, 192) if opts.resize_outputs else (512, 384)
-
-  res = get_unblurred(result_batch, transformed_image)
-
-  outputs_path = "/content/drive/MyDrive/whole_unblurred/"+str(i)+".jpg"
-  res.save(outputs_path)
-
-  # unblur whole_blur_30
-for i in range(91, 8144):
-#for i in range(8144):
-  image_path = EXPERIMENT_DATA_ARGS[experiment_type]["image_path"]+str(i)+".jpg"
-  #image_path = filename
-  original_image = Image.open(image_path).convert("RGB")
-  original_image = original_image.resize((192, 256))
-
-  original_image.resize((256, 256))
-
-
-  n_iters_per_batch = 5
-  opts.n_iters_per_batch = n_iters_per_batch
-  opts.resize_outputs = False  # generate outputs at full resolution
-
-  img_transforms = EXPERIMENT_ARGS['transform']
-  transformed_image = img_transforms(original_image) 
-
-  with torch.no_grad():
-    result_batch, result_latents, _ = run_inversion(transformed_image.unsqueeze(0).cuda(), 
-                                                    net, 
-                                                    opts,
-                                                    return_intermediate_results=True)
-  resize_amount = (256, 192) if opts.resize_outputs else (512, 384)
-
-  res = get_unblurred(result_batch, transformed_image)
-
-  outputs_path = "/content/drive/MyDrive/whole_unblurred_30/"+str(i)+".jpg"
-  res.save(outputs_path)
-
-
-  import os.path
-
-EXPERIMENT_DATA_ARGS = {
-    "cars": {
-        "model_path": "/content/drive/MyDrive/hyperstyle_cars.pt",
-        "w_encoder_path": "/content/drive/MyDrive/cars_w_encoder.pt",
-        "image_path": "/content/drive/MyDrive/important_blurred/",
-        "transform": transforms.Compose([
-            transforms.Resize((192, 256)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-    }
-}
-
-EXPERIMENT_ARGS = EXPERIMENT_DATA_ARGS[experiment_type]
-
-# unblur important neuron blur
-for i in range(8144):
-  image_path = EXPERIMENT_DATA_ARGS[experiment_type]["image_path"]+"blurred"+str(i)+".jpg"
-  if os.path.exists(image_path):
+  for i in range(8144):
+    image_path = EXPERIMENT_DATA_ARGS[experiment_type]["image_path"]+str(i)+".jpg"
     original_image = Image.open(image_path).convert("RGB")
-
     original_image = original_image.resize((192, 256))
 
-    original_image.resize((256, 256))
+    input_image = original_image
+    input_image.resize((256, 256))
 
 
     n_iters_per_batch = 5
@@ -142,16 +67,38 @@ for i in range(8144):
     opts.resize_outputs = False  # generate outputs at full resolution
 
     img_transforms = EXPERIMENT_ARGS['transform']
-    transformed_image = img_transforms(original_image) 
+    transformed_image = img_transforms(input_image) 
 
     with torch.no_grad():
-      result_batch, result_latents, _ = run_inversion(transformed_image.unsqueeze(0).cuda(), 
-                                                      net, 
-                                                      opts,
-                                                      return_intermediate_results=True)
-    resize_amount = (256, 192) if opts.resize_outputs else (512, 384)
+        tic = time.time()
+        result_batch, result_latents, _ = run_inversion(transformed_image.unsqueeze(0).cuda(), 
+                                                        net, 
+                                                        opts,
+                                                        return_intermediate_results=True)
+        toc = time.time()
+        print('Inference took {:.4f} seconds.'.format(toc - tic))
+        resize_amount = (256, 192) if opts.resize_outputs else (512, 384)
 
-    res = get_unblurred(result_batch, transformed_image)
+        res = get_unblurred(result_batch, transformed_image)
 
-    outputs_path = "/content/drive/MyDrive/important_unblurred/"+str(i)+".jpg"
-    res.save(outputs_path)
+        outputs_path = destination_path+str(i)+".jpg"
+        res.save(outputs_path)
+
+
+def get_unblurred(result_batch, transformed_image):
+    result_tensors = result_batch[0]  # there's one image in our batch
+    final_rec = tensor2im(result_tensors[-1]).resize(resize_amount)
+    return final_rec
+
+
+path_to_model = "/content/.../hyperstyle_cars.pt"
+path_to_w_encoder = "/content/.../cars_w_encoder.pt"
+
+whole_blurred_dataset_path = "/content/.../whole_blurred/"
+whole_blurred_destination = "/content/.../whole_unblurred/"
+
+unblur_with_hyperstyle(whole_blurred_dataset_path, whole_blurred_destination)
+
+important_blurred_dataset_path = "/content/.../important_blurred/"
+important_blurred_destination = "/content/.../important_unblurred/"
+unblur_with_hyperstyle(important_blurred_dataset_path, important_blurred_destination)
